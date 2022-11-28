a1ba88a9088f88b64b40267144cf448b32843b29
Lucas Hünniger <lucas.huenniger02@gmail.com>
2022-11-28 19:07:49 +0100

    chore: delete unnecessary comments

--- reddit_bot.py
+++ reddit_bot.py
@@ -6,12 +6,8 @@ import datetime
 # das hier mal anschauen, um meine Zugangsdaten verschlüsseln zu können
 reddit = praw.Reddit(client_id='qJsfx6IHun-VSHRQ8H2nOA',
                      client_secret='EBTx7u5EDeG2Cq0HX9E1cpu_y2vV5w',
-                    #  redirect_uri='http://localhost:8080',
                      refresh_token='1064866237953-I2RCpbVRKiLi5nM36wLi0X9jVCWT3Q',
                      user_agent='cool mensa-bot v1.0 by /u/MhouneyL',)
-# print(reddit.auth.url(scopes=["identity", "submit"], state="active", duration="permanent"))
-# refresh_token: 1064866237953-I2RCpbVRKiLi5nM36wLi0X9jVCWT3Q
-# print(reddit.auth.authorize('5Cdc9zf_5Ayl4eix2NplQz1z_XcVPg'))
 
 MENSA_IP = '836'
 currentDate = datetime.datetime.now()
@@ -35,5 +31,3 @@ for i in range(0, len(data_json)):
 '''
 
 reddit.subreddit(REDDIT_DHGE_NAME).submit(title, selftext=body, flair_id=FLAIR_ID_INFORMATION)
-
-# todo: Kasche fragen, ob er die Verwendung der Lamda-Functions einstellen kann, damit ich die für dieses Skript hier verwenden kann
