15c2f2b684ea3bc1872fef63975ab44643329a10
Lucas Hünniger <lucas.huenniger02@gmail.com>
2022-12-05 14:12:05 +0100

    fix: new point in wrong line

--- reddit_bot.py
+++ reddit_bot.py
@@ -20,11 +20,11 @@ def create_and_publish_reddit_post(data, date):
         meal_name = meal["name"]
         meal_price_students = meal["prices"]["students"]
         meal_notes = meal["notes"]
-        
-        body += f'# {meal_name}\n* Preis (Studenten): {meal_price_students}€\n*'
-        for j in range(0, len(meal_notes)):
-            body += f'{meal_notes[j]}\n'
 
+        body += f'# {meal_name}\n* Preis (Studenten): {meal_price_students}€\n'
+        for j in range(0, len(meal_notes)):
+            body += f'* {meal_notes[j]}\n'
+    
     reddit.subreddit(REDDIT_DHGE_NAME).submit(title, selftext=body, flair_id=FLAIR_ID_INFORMATION)
 
 ##### SCRIPT #####
