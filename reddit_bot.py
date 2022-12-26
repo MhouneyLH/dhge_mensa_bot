fd72f8388191ec83441a4bdbc563b56a582a756f
Lucas Hünniger <lucas.huenniger02@gmail.com>
2022-12-26 14:36:36 +0100

    chore: use consistent naming
    now I use camelCase for normal variables and SCREAMING_SNAKE_CASE for constants

--- reddit_bot.py
+++ reddit_bot.py
@@ -13,7 +13,7 @@ PRAW_REFRESH_TOKEN = '2418566552937-xh4rG2tMISfYHeWMJl9nF29-kYGTAw'
 PRAW_USER_AGENT = 'mensa-bot v1.1 by /u/MensaBot'
 
 ##### FUNCTIONS #####
-def create_and_publish_reddit_post(data, date):
+def createAndPublishRedditPost(data, date):
     global reddit
 
     title = f'Mittagessen vom {date.day}.{date.month}.{date.year}'
@@ -21,13 +21,13 @@ def create_and_publish_reddit_post(data, date):
 
     for i in range(0, len(data)):
         meal = data[i]
-        meal_name = meal["name"]
-        meal_price_students = meal["prices"]["students"]
-        meal_notes = meal["notes"]
+        mealName = meal["name"]
+        mealPriceStudents = meal["prices"]["students"]
+        mealNotes = meal["notes"]
 
-        body += f'# {meal_name}\n* Preis (Studenten): {meal_price_students:.2f}€\n'
-        for j in range(0, len(meal_notes)):
-            body += f'* {meal_notes[j]}\n'
+        body += f'# {mealName}\n* Preis (Studenten): {mealPriceStudents:.2f}€\n'
+        for j in range(0, len(mealNotes)):
+            body += f'* {mealNotes[j]}\n'
     
     reddit.subreddit(REDDIT_DHGE_NAME).submit(title, selftext = body, flair_id = REDDIT_FLAIR_ID_INFORMATION)
 
@@ -39,9 +39,9 @@ reddit = praw.Reddit(client_id = PRAW_CLIENT_ID,
 
 currentDate = datetime.datetime.now()
 currentDateInISOFormat = currentDate.isoformat()[0:10]
-api_url = f'https://openmensa.org/api/v2/canteens/{API_MENSA_IP}/days/{currentDateInISOFormat}/meals'
+API_URL = f'https://openmensa.org/api/v2/canteens/{API_MENSA_IP}/days/{currentDateInISOFormat}/meals'
 
-api_respose = urlopen(api_url)
-response_data = json.loads(api_respose.read())
+apiResponse = urlopen(API_URL)
+responseData = json.loads(apiResponse.read())
 
-create_and_publish_reddit_post(response_data, currentDate)
+createAndPublishRedditPost(responseData, currentDate)
